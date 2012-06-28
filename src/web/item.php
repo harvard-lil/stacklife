f5443ed2c53c22063a2f87ad53c5f1d78f141333
Annie Cain <anniejocain@gmail.com>
2012-06-28 15:12:22 -0400

    item data translator

--- src/web/item.php
+++ src/web/item.php
@@ -21,7 +21,7 @@ EOF;
 
 <script type="text/javascript">
 var worldcatnum = '',
-loc_sort_order = '';
+loc_call_num_sort_order = '';
 anchor_subject = '',
 uniform_id = '',
 uniform_count = '',
@@ -103,24 +103,18 @@ $(document).ready(function() {
     <div id="creator_container">
     {{{creators}}}
     </div>
-    <img class="cover-image ol-cover-image" src="http://covers.openlibrary.org/b/isbn/{{isbn}}-M.jpg" />
-    <a href="http://holliscatalog.harvard.edu/?itemid=|library/m/aleph|{{id_inst}}" target="_blank" class="button" id="hollis_button">HOLLIS</a>
-    <div id="online">
-      <a class="button-google-disabled" href="#viewerCanvas"><img src="<?php echo $www_root ?>/images/gbs_preview_disabled.png" /></a>
-      <a id="gviewer" class="button-google" href="#viewerCanvas" style="display:none;"><img src="<?php echo $www_root ?>/images/gbs_preview.png" border="0" /></a>
-    </div>
-    {{#if url}}
-    <a class="button" href="{{url}}">ONLINE</a>
-    {{/if}}
-    {{#if wp_url}}
-    <div class="wikipedia-icon"> 
-			<div class="wikipedia_link">
-				<a href="{{wp_url}}" target="_blank" >
-					<img src="<?php echo $www_root ?>/images/wikipedia.png" />
-				</a>
-			</div>
-		</div><!--end wikipedia-icon-->	
-		{{/if}}
+    <img class="cover-image" src="http://covers.openlibrary.org/b/isbn/{{isbn}}-M.jpg" />
+    <ul class="access">
+      <li><a href="http://holliscatalog.harvard.edu/?itemid=|library/m/aleph|{{id_inst}}" target="_blank">HOLLIS</a></li>
+      {{#if url}}
+      <li><a href="{{url}}">ONLINE</a></li>
+      {{/if}}
+      <li class="button-google-disabled"><a class="button-google-disabled" href="#viewerCanvas"><img src="<?php echo $www_root ?>/images/gbs_preview_disabled.png" /></a></li>
+      <li class="button-google"><a id="gviewer" class="button-google" href="#viewerCanvas" style="display:none;"><img src="<?php echo $www_root ?>/images/gbs_preview.png" border="0" /></a></li>
+      {{#if wp_url}}
+      <li><a href="{{wp_url}}" target="_blank" ><img src="<?php echo $www_root ?>/images/wikipedia.png" /></a></li>
+      {{/if}}
+    </ul>
 		<div id="availability-panel"></div>
 		
 		<div class="buy" style="display:none;">	 
---
+++ src/web/translators/item.php
@@ -0,0 +1,55 @@
+<?php
+    error_reporting(E_ALL ^ E_NOTICE);
+
+  require_once ('../../../etc/sl_ini.php');
+
+  $q = $_GET['query'];
+  $q = urlencode($q);
+  $offset = $_GET['start'];
+  $limit = $_GET['limit']; 
+  $search_type = $_GET['search_type'];
+
+  global $LIBRARYCLOUD_URL;
+
+  $url = "$LIBRARYCLOUD_URL?filter=$search_type:$q&limit=$limit&start=$offset";
+
+  // Get facets and filters
+  // TODO: This is ugly. Clean this stuff up.
+  $incoming = $_SERVER['QUERY_STRING'];
+  $facet_list = array();
+  foreach (explode('&', $incoming) as $pair) {
+      list($key, $value) = explode('=', $pair);
+      if ($key == 'facet') {
+          $url = $url . "&facet=" . $value;
+    }
+  }
+  
+  $filter_list = array();
+  $filter_string = '';
+    foreach (explode('&', $incoming) as $pair) {
+        list($key, $value) = explode('=', $pair);
+        if ($key == 'filter') {
+            $url = $url . "&filter=" . $value;
+      }
+    }
+    
+
+  $contents = fetch_page($url);
+    
+  echo $contents;
+
+function fetch_page($url) {
+
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, $url);
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+	$contents = curl_exec ($ch);
+	
+	curl_close ($ch);
+	
+	return $contents;
+}
+?>
