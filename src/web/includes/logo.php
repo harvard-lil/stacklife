23f429a1de2095ecc4817c1cb6050a3311260736
Annie Cain <anniejocain@gmail.com>
2012-10-17 11:24:20 -0400

    files for split header

---
+++ src/web/includes/logo.php
@@ -0,0 +1,7 @@
+<div class="header group">
+	<div class="header-content group">
+		<div class="logo">
+			<a href="<?php echo $www_root ?>/index.php">ShelfLife</a>
+  	</div><!--end logo-->
+	</div><!--end header-content-->
+</div><!--end header-->
